a4f1c7f20698da95b562658e366f84fdff5578a1
jiaoyulong <975887905@qq.com>
2017-08-18 17:15:29 +0800

    20170818

---
+++ src/components/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+      <nav class="nav-header">
+        <a href="/" class="logo">迪迪贷-vue版本</a>
+        <a href="/" class="avatar"></a>
+      </nav>
+      <nav class="home-nav">
+        <router-link
+          v-for = "item in type"
+          to="/list01"
+          @click="openBookCategory(item.num)"
+        >
+          <i class="icon icon-sort"></i>
+          <span>{{item.word}}</span>
+        </router-link>
+      </nav>
+      <loading v-show="loading"></loading>
+  </div>
+</template>
+
+<script>
+//  import axios from 'axios'
+//  import BookList from './Common/BookList.vue'
+  import Loading from './Loading/Loading.vue'
+
+  export default {
+    data () {
+      return {
+        booklist: [],
+        type: [
+          {
+            num: 1,
+            word: '供应链'
+          },
+          {
+            num: 2,
+            word: '车贷'
+          },
+          {
+            num: 3,
+            word: '优企贷'
+          },
+          {
+            num: 4,
+            word: '体验标'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// axios vue更新到2.0之后，作者就宣告不再对vue-resource更新，而是推荐的axios
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/css/reset.css'
+import Common from '@/assets/js/utils.js'
+
+Vue.use(Common)
 
 Vue.config.productionTip = false
 
